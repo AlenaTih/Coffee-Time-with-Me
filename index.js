000daf425d7c2840edc2c841d2eb21d65e48ae48
Alena <alyonatihomirova9@gmail.com>
2023-12-25 19:31:45 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// JavaScript
+const submitButton = document.getElementById("submit-button")
+const inputName = document.getElementById("input-name")
+
+submitButton.addEventListener("click", function() {
+    
+    const form = document.querySelector("form")
+
+    if (!form.checkValidity()) {
+        // If the form is not valid, prevent submition
+        // The form is valid when the name and email input fields are not empty
+        alert("Please type in your name and email ❤️")
+    } else {
+        console.log("clicked")
+        alert(`Thank you, ${inputName.value} ❤️ I will contact you!`)
+    }
+})
